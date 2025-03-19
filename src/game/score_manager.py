fcb99340f979ec5b6c07c1591f5c33bc928c3ede
Tran Minh Duc <tmduc2606@gmail.com>
2025-03-19 23:18:17 +0700

    Update Score class
    - Minor fixes in calculating multiplier
    - Update combo evaluation

--- src/game/score_manager.py
+++ src/game/score_manager.py
@@ -1,5 +1,5 @@
 class Score:
-    def __init__(self, score, multiplier, combo, time_added):
+    def __init__(self):
         self.score = 0
         self.multiplier = 1
         self.combo = 1
@@ -8,10 +8,12 @@ class Score:
     #Score management
     def add_score(self):
         if self.combo == 1 or self.combo == 2:
+            self.multiplier = 1
             self.score += 15
         elif self.combo >= 3:
-            self.multiplier = 1 + 0.5 * (self.combo - 3)
-            self.score += 15 * self.multiplier
+            self.multiplier = 1 + 0.5 * (self.combo - 2)
+            self.score += int(15 * self.multiplier)
+
     def reset_score(self):
         self.score = 0
         self.multiplier = 1
@@ -27,9 +29,34 @@ class Score:
         if self.combo >= 3: #Must start at 3rd consecutive combo
             index = min(self.combo - 3, len(default_time_bonus) - 1)
             self.time_added += default_time_bonus[index]
-        #else:
-        #    self.time_added = 10
 
-    def update_combo(self):
-        #... When the game_manager class complete, keep on the trigger system
-        self.combo += 1
+    def update_combo(self, matched):
+        #If it's matched -> Update the combo. Else reset the combo if mismatches or time runs out (Soon)
+        if matched:
+            self.combo = min(self.combo + 1, 10)
+        else:
+            self.combo = 1
+
+
+def test_score():
+    print("Initializing Score System...")
+    score = Score()
+
+    #Multi-complex score evalution
+    for i in range(6):
+        score.update_combo(True)
+        score.add_score()
+        score.apply_time_bonus()
+
+    print(f"Total score: {score.return_score()}")
+    print(f"Total combos: {score.combo}")
+
+    score.update_combo(False)
+    score.add_score()
+    score.apply_time_bonus()
+
+    print(f"Total score: {score.return_score()}")
+    print(f"Total combos: {score.combo}")
+
+if __name__ == "__main__":
+    test_score()
